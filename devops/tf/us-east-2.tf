fe082d31e0375edfb694e05ec6273107c4e0dbf2
Talha Ahmed <talha5389@gmail.com>
2024-02-01 23:52:30 +0500

    fix: ip

--- devops/tf/us-east-2.tf
+++ devops/tf/us-east-2.tf
@@ -3,7 +3,7 @@ module "us_east_2" {
   deployment_name                    = local.deployment_name
   docker_general_instance_role_name  = data.terraform_remote_state.infrastructure.outputs.us_east_2.docker_general_role.name
   docker_general_portainer_user_name = data.terraform_remote_state.infrastructure.outputs.us_east_2.docker_general_portainer_user.name
-  container_ip                       = "3.138.118.178"
+  container_ip                       = "3.128.200.223"
   #   container_ip = data.terraform_remote_state.infrastructure.outputs.us_east_2.docker_general_ec2.public_ip
 
   environment       = var.environment
