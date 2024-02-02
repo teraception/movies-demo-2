a62aeb52f4a3d6c85fc4058fd761c086c44d7e3c
Shafqat Ullah <shafkatullah@teraception.com>
2024-02-02 23:27:02 +0500

    fix: docker args

--- devops/docker/deps/build-images.sh
+++ devops/docker/deps/build-images.sh
@@ -4,6 +4,10 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 docker build $DOCKER_BUILD_PARAMS \
     $COMMON_ARGS \
     $COMMON_DOCKER_CACHE_SOURCES \
+     --build-arg COGNITO_CLIENT_ID=$COGNITO_CLIENT_ID \
+     --build-arg COGNITO_POOL_ID=$COGNITO_POOL_ID \
+     --build-arg AWS_REGION=$AWS_REGION \
+    $COMMON_DOCKER_CACHE_SOURCES \
     -f devops/docker/deps/Dockerfile \
     -t $imageName \
     .
